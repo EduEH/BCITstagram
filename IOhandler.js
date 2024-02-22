a21ee77484e6ca72ee022a81f4d69d765fe4f86d
EduEH <eduardo_hirai@hotmail.com>
2024-02-22 03:41:20 -0800

    Finalized

--- IOhandler.js
+++ IOhandler.js
@@ -4,11 +4,12 @@
  * Description: Collection of functions for files input/output related operations
  *
  * Created Date:
- * Author:
+ * Author: Eduardo Eriyo Hirai
  *
  */
 
-const yauzl = require("yauzl-promise"), //CHANGE YO YAUZL. Chekc documentation to see how works
+const { pipeline } = require("stream/promises");
+const yauzl = require("yauzl-promise"),
   fs = require("fs"),
   PNG = require("pngjs").PNG,
   path = require("path");
@@ -34,10 +35,11 @@ const unzip = async (pathIn, pathOut) => {
         await pipeline(readStream, writeStream);
       }
     }
+    console.log("Extraction operation complete");
   } finally {
     await zip.close();
   }
-}; //PATHiN IS WHERE FILES ARE, out is where they go
+}; 
 
 /**
  * Description: read all the png files (ONLY PNG) from given directory and return Promise containing array of each png file path
@@ -45,7 +47,23 @@ const unzip = async (pathIn, pathOut) => {
  * @param {string} path
  * @return {promise}
  */
-const readDir = (dir) => {};
+const readDir = (dir) => {
+  return new Promise((resolve, reject)=>{
+    fs.readdir(dir, (err, images) => {
+      if (err) {
+        reject(err)
+      } else {
+        let imagesList = []
+        for(const file of images){
+          if(path.extname(file) == ".png"){  
+            imagesList.push(`${__dirname}/unzipped/${file}`)
+          }
+        }
+        resolve(imagesList)
+      }
+    })
+  })
+};
 
 /**
  * Description: Read in png file by given pathIn,
@@ -55,13 +73,58 @@ const readDir = (dir) => {};
  * @param {string} pathProcessed
  * @return {promise}
  */
-const grayScale = (pathIn, pathOut) => {};
-//pngjs
-//In this.data, "this" refers to your image, equivalent to self
-//var idx = (this.width * y + x) << 2
+const grayScale = (pathIn, pathOut) => {
+  for (const file of pathIn){
+    fs.createReadStream(file)
+  .pipe(
+    new PNG({
+      filterType: 4,
+    })
+  )
+  .on("parsed", function () {
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        const segment = (y * 4) * this.width + x * 4;
+        const avg = (this.data[segment] + this.data[segment + 1] + this.data[segment + 2]) / 3;
+        this.data[segment] = avg;
+        this.data[segment + 1] = avg;
+        this.data[segment + 2] = avg;
+      }
+    }
+    const filePath = file.split("/");
+    this.pack().pipe(fs.createWriteStream(`${pathOut}/${filePath.slice(-1).pop()}`));
+  });
+  }
+};
 
+const sepia = (pathIn, pathOut) => {
+  for (const file of pathIn){
+    fs.createReadStream(file)
+  .pipe(
+    new PNG({
+      filterType: 4,
+    })
+  )
+  .on("parsed", function () {
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        const idx = (this.width * y + x) << 2;
+        const r = this.data[idx];
+        const g = this.data[idx + 1];
+        const b = this.data[idx + 2];
+        this.data[idx] = Math.min(255, (0.393 * r + 0.769 * g + 0.189 * b));
+        this.data[idx + 1] = Math.min(255, (0.349 * r + 0.686 * g + 0.168 * b));
+        this.data[idx + 2] = Math.min(255, (0.272 * r + 0.534 * g + 0.131 * b));
+      }
+    }
+    const filePath = file.split("/");
+    this.pack().pipe(fs.createWriteStream(`${pathOut}/${filePath.slice(-1).pop()}`));
+  });
+  }
+};
 module.exports = {
   unzip,
   readDir,
   grayScale,
+  sepia,
 };
--- main.js
+++ main.js
@@ -1,19 +1,45 @@
 const path = require("path");
+const readline = require("readline");
+const { EOL } = require('os');
 /*
  * Project: Milestone 1
  * File Name: main.js
  * Description:
  *
  * Created Date:
- * Author:
+ * Author: Eduardo Eriyo Hirai
  *
  */
 
 const IOhandler = require("./IOhandler"); //All logic goes here
 const zipFilePath = path.join(__dirname, "myfile.zip");
 const pathUnzipped = path.join(__dirname, "unzipped");
-const pathProcessed = path.join(__dirname, "grayscaled");
+const pathGrayscale = path.join(__dirname, "grayscaled");
+const pathSepia = path.join(__dirname, "sepia");
 
-IOhandler.unzip(zipFilePath, pathUnzipped); //Added in class
+async function main() {
+    try{
+        await IOhandler.unzip(zipFilePath, pathUnzipped);
+        const files = await IOhandler.readDir(pathUnzipped);
+        const user = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        user.question(`Which filter do you want to apply?${EOL}1-Grayscale${EOL}2-Sepia${EOL}`, function(answer) {
+            if (answer == 1) {
+                IOhandler.grayScale(files, pathGrayscale);
+                user.close();
+            } else if (answer == 2) {
+                IOhandler.sepia(files, pathSepia);
+                user.close();
+            } else {
+                console.log("Invalid option.")
+                user.close();
+            }
+        });
+    } catch(err) {
+        console.log(err);
+    }
+}
 
-//Add question asking for which filter to use (Available filters:)
+main();
